975b6aab93963638fd98057a6274c0d340f4d498
Kornpong Methawanitpong <kmethawa@z1t7c3.42bangkok.com>
2021-10-24 15:01:36 +0700

    add evaluate folder

---
+++ evaluate/npatthan-c-00/ex06/ft_print_comb2.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npatthan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 16:26:10 by npatthan          #+#    #+#             */
+/*   Updated: 2021/10/24 14:14:57 by kmethawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_f_s_l(int f, int s, int l)
+{
+	ft_putchar((f / 10) + '0');
+	ft_putchar((f % 10) + '0');
+	ft_putchar(' ');
+	ft_putchar((s / 10) + '0');
+	ft_putchar((s % 10) + '0');
+	if (l != 1)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+	int	l;
+
+	a = 0;
+	while (a < 100)
+	{
+		b = a + 1;
+		while (b < 100)
+		{
+			if (a == 98 && b == 99)
+			{
+				l = 1;
+			}
+			else
+			{
+				l = 0;
+			}
+			print_f_s_l(a, b, l);
+			b++;
+		}
+		a++;
+	}
+}
+
+int	main()
+{
+	ft_print_comb2();
+}
---
+++ evaluate/npatthan-c-00/ex07/ft_putnbr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npatthan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 16:58:05 by npatthan          #+#    #+#             */
+/*   Updated: 2021/10/24 14:16:38 by kmethawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(48 + nb % 10);
+	}
+}
+
+int	main()
+{
+	ft_putnbr(42);
+}
