81afef18538d968cebf6f8ac091b716f626753cd
Kornpong Methawanitpong <kmethawa@z1t7c3.42bangkok.com>
2021-11-07 22:17:43 +0700

    add rush-02

---
+++ ft/test/test_rev4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char	*ft_rev_print(const char *str, int i)
+{
+	const char *p = str;
+
+	while (*p)
+		++p;
+	while (p != str)
+		putchar(*--p);
+	return (char *)str;
+}
+
+int	main(void)
+{
+	ft_rev_print("test");
+	putchar('\n');
+	return (0);
+}
